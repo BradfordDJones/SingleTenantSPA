6ce57be182780cdcae704d578a85e7ff3a2c6b37
Bradford Jones <bradford_jones@hotmail.com>
2024-09-07 17:25:04 -0400

    Initial checkin full demo functionality

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const argv = require('yargs')
+    .usage('Usage: $0 -p [port]')
+    .alias('p', 'port')
+    .describe('port', '(Optional) Port Number - Default is 3000')
+    .strict()
+    .argv;
+
+const DEFAULT_PORT = 3000;
+
+const app = express();
+
+let port = DEFAULT_PORT;
+if (argv.port) {
+    port = argv.p;
+}
+
+app.use(morgan('dev'));
+app.use("/lib", express.static(path.join(__dirname, '../../lib/msal-browser/lib')));
+
+app.use(express.static('app'));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port);
+console.log(`Listening on port ${port}...`);
